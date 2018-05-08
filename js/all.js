46294a75a4dc9883abf97fd04e4d11dad3a2801b
hobbyling <g99949@hotmail.com>
2018-05-08 11:00:31 +0800

    modify madel

--- js/all.js
+++ js/all.js
@@ -121,11 +121,13 @@ xhr.onload = function(){
             spotWebsite_has = '<div class="content col-12 col-md-6"><img src="img/icons_website.png" alt=""><span class="website"> <a target="_blank" href='+ spotWebsite +'> 官方網站</a></span></span></div>';
           }
 
-          var str = '<div class="modalImg"><img src="'+ spotImg +'" alt=""></div><div class="modalInfo row justify-content-center"><div class="description col-12">'+ spotDescription +'</div><div class="col-12 row"><div class="content col-12 col-md-6"><img src="img/icons_clock.png" alt=""><span class="opentime"> '+ spotTime +'</span></div><div class="content col-12 col-md-6"><img src="img/icons_phone.png" alt=""><span class="tel"> '+ spotTel +'</span></div><div class="content col-12 col-md-6"><img src="img/icons_tag.png" alt=""><span class="ticketinfo"> '+ spotTicket +'</span></div>'+ spotWebsite_has +'<div class="content col-12 col-md-6"><img src="img/icons_pin.png" alt=""><span class="add"> '+ spotAdd +'</span></div><div class="content col-12 col-md-6"><img src="img/icons_remark.png" alt=""><span class="remark"> '+ spotRemark +'</div></div><div class="map col-12" id="'+ chooseId +'"></div></div></div>';
+          // var str = '<div class="modalImg"><img src="'+ spotImg +'" alt=""></div><div class="modalInfo row justify-content-center"><div class="description col-12"><img src="img/icons_book.png" alt="">'+ spotDescription +'</div><div class="col-12 row"><div class="content col-12 col-md-6"><img src="img/icons_clock.png" alt=""><span class="opentime"> '+ spotTime +'</span></div><div class="content col-12 col-md-6"><img src="img/icons_phone.png" alt=""><span class="tel"> '+ spotTel +'</span></div><div class="content col-12 col-md-6"><img src="img/icons_tag.png" alt=""><span class="ticketinfo"> '+ spotTicket +'</span></div>'+ spotWebsite_has +'<div class="content col-12 col-md-6"><img src="img/icons_pin.png" alt=""><span class="add"> '+ spotAdd +'</span></div><div class="content col-12 col-md-6"><img src="img/icons_remark.png" alt=""><span class="remark"> '+ spotRemark +'</div></div><div class="map col-12" id="'+ chooseId +'"></div></div></div>';
+
+          var str = '<div class="modalImg"><img src="'+ spotImg +'" alt=""></div><div class="modalInfo row"><div class="content col-12 col-md-12"><img src="img/icons_book.png" alt=""><span class="description">'+ spotDescription +'</span></div><div class="content col-12 col-md-6"><img src="img/icons_clock.png" alt=""><span class="opentime"> '+ spotTime +'</span></div><div class="content col-12 col-md-6"><img src="img/icons_phone.png" alt=""><span class="tel"> '+ spotTel +'</span></div><div class="content col-12 col-md-6"><img src="img/icons_tag.png" alt=""><span class="ticketinfo"> '+ spotTicket +'</span></div>'+ spotWebsite_has +'<div class="content col-12 col-md-6"><img src="img/icons_pin.png" alt=""><span class="add"> '+ spotAdd +'</span></div><div class="content col-12 col-md-6"><img src="img/icons_remark.png" alt=""><span class="remark"> '+ spotRemark +'</div><div class="map col-12" id="'+ chooseId +'"></div></div></div>';
 
           modalTitle.innerHTML = spotName + '('+ spotZone +')';
           modalbody.innerHTML = str;
-          initMap(spotLat, spotLng, chooseId);
+          initMap(spotLat, spotLng, chooseId,spotName);
         }
       }
     }
@@ -133,20 +135,31 @@ xhr.onload = function(){
 
     //---google地圖---
     var map
-    function initMap(spotlat,spotlng,spotid){
+    function initMap(spotlat,spotlng,spotid,spotName){
 
       var mapLat = Number(spotlat);
       var mapLng = Number(spotlng);
 
       map = new google.maps.Map(document.getElementById(spotid),{
         zoom: 15,
-        center:{ lat:mapLat, lng:mapLng}
+        center:{ lat:mapLat, lng:mapLng},
+        styles:[{"featureType":"landscape","stylers":[{"hue":"#FFBB00"},{"saturation":43.400000000000006},{"lightness":37.599999999999994},{"gamma":1}]},{"featureType":"road.highway","stylers":[{"hue":"#FFC200"},{"saturation":-61.8},{"lightness":45.599999999999994},{"gamma":1}]},{"featureType":"road.arterial","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":51.19999999999999},{"gamma":1}]},{"featureType":"road.local","stylers":[{"hue":"#FF0300"},{"saturation":-100},{"lightness":52},{"gamma":1}]},{"featureType":"water","stylers":[{"hue":"#0078FF"},{"saturation":-13.200000000000003},{"lightness":2.4000000000000057},{"gamma":1}]},{"featureType":"poi","stylers":[{"hue":"#00FF6A"},{"saturation":-1.0989010989011234},{"lightness":11.200000000000017},{"gamma":1}]}]
+      });
+
+      var contentString = '<div>'+ spotName +'</div>';
+      var infowindow = new google.maps.InfoWindow({
+        content: contentString
       });
 
       var marker = new google.maps.Marker({
         position: {lat:mapLat, lng:mapLng},
         map:map
-      })
+      });
+
+      marker.addListener('click', function() {
+        infowindow.open(map, marker);
+      });
+
     }
 
     //---gotop---
